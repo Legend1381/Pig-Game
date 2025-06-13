3ecd0dc957f89644c4bd24d521aaf0ec323bf126
Legend1381 <mostafa.nazari9494@gmail.com>
2025-06-13 12:00:25 +0330

    add switching the active player logic

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const palyer0Element = document.querySelector(".player--0")
+const palyer1Element = document.querySelector(".player--1");
 const score0Element = document.getElementById("score--0");
 const score1Element = document.getElementById("score--1");
 const current0Element = document.getElementById("current--0");
@@ -9,8 +11,9 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-
+const totalScores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 diceElement.classList.add("hidden");
 
@@ -22,7 +25,12 @@ btnRoll.addEventListener("click", () => {
 
   if (dice !== 1) {
     currentScore += dice;
-    current0Element.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    palyer0Element.classList.toggle("player--active")
+    palyer1Element.classList.toggle("player--active")
   }
 });
